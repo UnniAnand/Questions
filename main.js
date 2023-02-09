b55a4fa727fa96914a971d79dc669df9e76a8d2b
UnniAnand <124883192+UnniAnand@users.noreply.github.com>
2023-02-09 16:39:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+function button1(){
+     document.getElementById("btn1").innerHTML="What's the craziest thing you have ever done?";
+     
+}
+function button2(){
+    document.getElementById("btn2").innerHTML="Which celebrity would you pick to exchange lives with and why?";
+}
+function button3(){
+    document.getElementById("btn3").innerHTML="If you could effortlessly pick up a skill/talent in an instant, What would it be?";
+}
+function button4(){
+    document.getElementById("btn4").innerHTML="If you could choose your age forever, What age would you choose and why?";
+}
+function button5(){
+    document.getElementById("btn5").innerHTML="If you could be any movie, What would it be and what character would you play?";
+}
+function button6(){
+    document.getElementById("btn6").innerHTML="What is your favorite time of the day and why?";
+}
+function button7(){
+    document.getElementById("btn7").innerHTML="If you were a superhero what would your superpower be and why?";
+}
+function button8(){
+    document.getElementById("btn8").innerHTML="If you had to delete all but thee apps from your smartphone, which one's would it be?";
+}
+function button9(){
+    document.getElementById("btn9").innerHTML="If you could go anywhere in the world, Where would you go and why?";
+}
+function button10(){
+    document.getElementById("btn10").innerHTML="What would you do if you win a billion dollar?";
+}
+function change1(){
+    document.getElementById("btn1").innerHTML="1"
+}
+function change2(){
+    document.getElementById("btn2").innerHTML="2"
+}
+function change3(){
+    document.getElementById("btn3").innerHTML="3"
+}
+function change4(){
+    document.getElementById("btn4").innerHTML="4"
+}
+function change5(){
+    document.getElementById("btn5").innerHTML="5"
+}
+function change6(){
+    document.getElementById("btn6").innerHTML="6"
+}
+function change7(){
+    document.getElementById("btn7").innerHTML="7"
+}
+function change8(){
+    document.getElementById("btn8").innerHTML="8"
+}
+function change9(){
+    document.getElementById("btn9").innerHTML="9"
+}
+function change10(){
+    document.getElementById("btn10").innerHTML="10"
+}
